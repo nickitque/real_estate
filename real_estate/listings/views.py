2a0c375af6cdd91374e15c8175e81b83a1427089
Nikita Radzisheuski <nickitque.39@gmail.com>
2022-08-18 16:24:05 +0300

    Updated urls and views. Added method listing_retrieve

--- real_estate/listings/views.py
+++ real_estate/listings/views.py
@@ -1,3 +1,23 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import Listing
 
 # Create your views here.
+
+# CRUD - Create, retrieve, update, delete, list
+
+
+def listing_list(request):
+    listings = Listing.objects.all()
+    context = {
+        "listings": listings
+    }
+    return render(request, "listings.html", context)
+
+
+def listing_retrieve(request, pk):
+    listing = Listing.objects.get(id=pk)
+    context = {
+        "listing": listing
+    }
+    return render(request, "listing.html", context)
